de79eddd00ec43262a01a5ac3562daf439c35056
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2020-09-19 18:31:10 -0400

    modified files

--- timey.py
+++ timey.py
@@ -1,6 +1,7 @@
 from flask import Flask , jsonify , request
 import cv2
 import numpy
+import File
 
 app = Flask(__name__)
 
@@ -11,7 +12,9 @@ def helloworld():
 
 @app.route('/check_face_exists' , methods=['POST'])
 def checkImage():
-    image = cv2.imdecode(numpy.fromstring(request.files['file'].read(), numpy.uint8), cv2.IMREAD_UNCHANGED)
+    new_file = request.files['file']
+    npimg = numpy.fromfile(new_file , numpy.unit8)
+    image = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     faces = faceCascade.detectMultiScale(
@@ -28,13 +31,18 @@ def checkImage():
         cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color) 
 	'''
 
-    if len(faces) > 0:
+    if len(faces) <= 0:
         return {
             "face_exists": True
         }
     else:
-        return {
-            "face_exists":False
-        }
+        for(x , y  , w , h) in faces:
+            cv2.rectangle(image , (x , y) , (x + w , y + h) , (0 , 255 , 0))
+            new_image = image[y:y + h, x:x + w]
+
+            
+
+
+
 
 
