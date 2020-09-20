0f483d709f08a0a4eadbf96c52d67dafc2ec15c0
Ashwin_Pokharel <pokharel.ashwin1@gmail.com>
2020-09-20 03:41:29 -0400

    modified api

--- face_detector.py
+++ face_detector.py
@@ -4,11 +4,11 @@ import sys
 imagePath = sys.argv[1]
 
 image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2image)
 
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 faces = faceCascade.detectMultiScale(
-    gray,
+    image,
     scaleFactor=1.3,
     minNeighbors=3,
     minSize=(30, 30)
--- timey.py
+++ timey.py
@@ -1,9 +1,18 @@
 from flask import Flask , jsonify , request
 import cv2
 import numpy
-import File
+from PIL import Image
+from io import BytesIO
+from flask_cors import CORS
+from decouple import config
+import requests
+import tensorflow as tf
+import json
 
 app = Flask(__name__)
+CORS(app)
+
+classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
 
 @app.route('/hello' , methods=['POST'])
 def helloworld():
@@ -12,35 +21,56 @@ def helloworld():
 
 @app.route('/check_face_exists' , methods=['POST'])
 def checkImage():
-    new_file = request.files['file']
-    npimg = numpy.fromfile(new_file , numpy.unit8)
-    image = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blob_string = request.files['file'].read()
+    if(len(blob_string) < 100):
+         return jsonify({
+            "face_exists":False
+        })
+    try:
+        image = cv2.imdecode(numpy.fromstring(blob_string, dtype=numpy.uint8), cv2.IMREAD_GRAYSCALE)
+    except Exception as e:
+        return jsonify({
+            "face_exists":False
+        })
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
     faces = faceCascade.detectMultiScale(
-        gray,
+        image,
         scaleFactor=1.3,
         minNeighbors=3,
-        minSize=(30, 30)
+        minSize=(30, 30) 
     )
-    '''
-    for (x , y , w , h) in faces:
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi_color = image[y:y + h, x:x + w] 
-        print("[INFO] Object found. Saving locally.") 
-        cv2.imwrite(str(w) + str(h) + '_faces.jpg', roi_color) 
-	'''
-
+    
     if len(faces) <= 0:
-        return {
-            "face_exists": True
-        }
-    else:
-        for(x , y  , w , h) in faces:
-            cv2.rectangle(image , (x , y) , (x + w , y + h) , (0 , 255 , 0))
-            new_image = image[y:y + h, x:x + w]
+         return jsonify({
+            "face_exists":False
+        })
 
-            
+    x , y , w , h = faces[0]
+    face_image = image[y:y + h, x:x + w]
+    resized_image = cv2.resize(face_image , (48 , 48) , interpolation=cv2.INTER_NEAREST)
+    image = numpy.array(resized_image)
+    image = numpy.expand_dims(image ,axis=2)
+    print(image.shape)
+    data = json.dumps({
+        "instances": [image.tolist()]
+    })
+    headers = {"content_type":"application/json"}
+    r = requests.post(config('TENSOR_URL') , data=data , headers=headers)
+    tensor_response = r.json()
+    tensor_response = tensor_response['predictions']
+    score = tf.nn.softmax(tensor_response[0])
+    label = classes[numpy.argmax(score)]
+    certainty = numpy.max(score)
+    return jsonify({
+            "face_exists":True,
+            "emotion":label,
+            "certainty":str(certainty)
+    })
+
+
+
+        
+
 
 
 
